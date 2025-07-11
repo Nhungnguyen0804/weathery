b7e8c5aaca7dbf29f855c65c21cf3e9097f6ab0c
Nhung Nguyen <nhungnguyen8404@gmail.com>
2025-07-11 18:19:29 +0700

    create global style css file

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
+import requests
+from flask_cors import CORS
+
+
+load_dotenv()
 
 app = Flask(__name__)
-
+CORS(app)
+API_KEY = os.getenv("WEATHER_API_KEY")
+print(API_KEY)
 @app.route('/weather')
 def create_local_api():
-    return 'hello'
+    url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q=Vietnam"
+    response = requests.get(url)
+    return jsonify(response.json())
 
 if __name__ == "__main__":
     app.run(debug=True)
